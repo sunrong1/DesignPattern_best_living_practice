5081e8a817d0fc7f27cc0edc29725fd70e29c0d3
dave <sunrong1990@126.com>
2022-03-13 23:58:56 +0800

    增加原型模式

---
+++ Prototype/Program.cs
@@ -0,0 +1,60 @@
+﻿using Prototype;
+/// <summary>
+/// 场景1:考试试卷的复制，简历的复制，直接初始化复制成本高，能否快速复制？
+/// 
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// 原始方法,修改比较困难，直接重新创建代价高
+    /// </summary>
+    static void Main1()
+    {
+        Resume a = new Resume("dave", "22");
+        a.SetWorkExprience("多年项目经验");
+
+        Resume b = a;
+
+        b.SetWorkExprience("丰富的项目经验");
+        a.Display();
+        b.Display();
+
+
+    }
+
+    /// <summary>
+    /// 克隆方法，浅复制
+    /// </summary>
+    static void Main2()
+    {
+        Resume a = new Resume("dave", "22");
+        a.SetWorkExprience("多年项目经验");
+
+        Resume b = (Resume)a.Clone();
+
+        b.SetWorkExprience("丰富的项目经验");
+        a.Display();
+        b.Display();
+    }
+
+    /// <summary>
+    /// 克隆方法，深复制
+    /// </summary>
+    static void Main()
+    {
+        Resume a = new Resume("dave", "22");
+        a.SetWorkExprience("多年项目经验");
+        a.SetSchool("上海大学","延长校区");
+        Resume b = (Resume)a.Clone();
+
+        b.SetWorkExprience("丰富的项目经验");
+        b.SetSchool("清华大学", "北京校区");
+        a.Display();
+        b.Display();
+        
+        //深度复制
+        Resume c = (Resume)a.Clone2();
+        c.SetSchool("北京大学", "北京校区");
+        c.Display();
+    }
+}
---
+++ Prototype/Resume.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Resume:ICloneable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Age { get; set; }
+
+        SchoolInfo schoolInfo = new SchoolInfo();
+        public Resume(string name,string age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void SetWorkExprience(string description)
+        {
+            this.Description = description;
+        }
+
+        public void SetSchool(string schoolName,string schoolDescription)
+        {
+            schoolInfo.Name = schoolName;
+            schoolInfo.Description = schoolDescription;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("信息：" +Age+ Description+ Name);
+            Console.WriteLine("学校：" + schoolInfo.Name);
+        }
+
+        public object Clone()
+        {
+           return this.MemberwiseClone();
+        }
+
+        public object Clone2()
+        {
+            Resume resume = new Resume(this.Name,this.Age);
+            resume.schoolInfo= (SchoolInfo)schoolInfo.Clone();
+            resume.SetWorkExprience(this.Description);
+            return resume;
+        }
+    }
+}
